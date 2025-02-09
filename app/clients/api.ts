2c644d90de7650e47e160b420f7eb94f6c582063
shubhswain45 <shubhkantswain@.com>
2025-02-09 10:53:08 +0530

    iiiii

--- app/clients/api.ts
+++ app/clients/api.ts
@@ -2,11 +2,12 @@ import { GraphQLClient } from "graphql-request";
 
 export const createGraphqlClient = (token?: string) => {
     const Token = token || ""
-    
+
     return new GraphQLClient('https://remix-project-server.onrender.com/graphql', {
         credentials: "include",
-        headers: () => ({
+        headers: {
             Authorization: `Bearer ${Token}`
-          }),
+        },
     });
+
 }
