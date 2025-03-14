b0d38c5ba350d67d90080a84c9fa343777096796
shubhswain45 <shubhkantswain@.com>
2025-03-14 07:49:39 +0530

    ooo

--- app/routes/_app.collection.tracks/route.tsx
+++ app/routes/_app.collection.tracks/route.tsx
@@ -194,7 +194,7 @@ const LikedTracks = () => {
                             <div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 rounded">
                               {/* <Play className="w-5 h-5 text-white" /> */}
                               <button className="cursor-pointer">
-                                {track?.id == trackDetails.id && trackDetails.isPlaying ? (
+                                {(track?.id == trackDetails.id && trackDetails.isPlaying && initialized) ? (
                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                     <defs>
                                       <path id="ic_playback_pause-a" d="M9,22 L6,22 C5.45,22 5,21.55 5,21 L5,3 C5,2.45 5.45,2 6,2 L9,2 C9.55,2 10,2.45 10,3 L10,21 C10,21.55 9.55,22 9,22 Z M19,21 L19,3 C19,2.45 18.55,2 18,2 L15,2 C14.45,2 14,2.45 14,3 L14,21 C14,21.55 14.45,22 15,22 L18,22 C18.55,22 19,21.55 19,21 Z"></path>
