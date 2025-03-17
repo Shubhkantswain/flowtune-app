13f75f60e89d2497c9173f6f1132a871a27b795d
shubhswain45 <shubhkantswain@.com>
2025-03-17 11:08:00 +0530

    pp

--- app/routes/_app/_components/playbackcontroller/LeftTrackInfo.tsx
+++ app/routes/_app/_components/playbackcontroller/LeftTrackInfo.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTrackStore } from '~/store/useTrackStore';
-import PlaceholderTrack from '../PlaceholderTrack';
 
 type LeftTrackInfoProps = {
     setIsOpen: (isOpen: boolean) => void;
@@ -16,12 +15,12 @@ const LeftTrackInfo: React.FC<LeftTrackInfoProps> = ({ setIsOpen }) => {
     };
 
     const isTrackSelected = Boolean(trackDetails.id);
-    
+
     return (
         <article className="flex items-center flex-1 min-w-0 md:w-1/3">
-            <button 
+            <button
                 className={`
-                    w-12 h-12 bg-gray-800 rounded-md overflow-hidden mr-3 
+                    w-12 h-12 rounded-md overflow-hidden mr-3 
                     flex-shrink-0 relative group 
                     ${isTrackSelected ? "cursor-pointer" : "cursor-default"}
                 `}
@@ -35,12 +34,26 @@ const LeftTrackInfo: React.FC<LeftTrackInfoProps> = ({ setIsOpen }) => {
                         className="w-full h-full object-cover group-hover:opacity-75 transition-opacity"
                     />
                 ) : (
-                    <PlaceholderTrack index={1} />
+                    <div className="w-full h-full flex items-center justify-center bg-[#313232] rounded-md">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24" height="24"
+                            viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor"
+                            strokeWidth="2" strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="w-8 h-8 text-gray-300"
+                        >
+                            <circle cx="8" cy="18" r="4" />
+                            <path d="M12 18V2l7 4" />
+                        </svg>
+                    </div>
+
                 )}
-                
+
                 {isTrackSelected && (
                     <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity bg-black/40 group-hover:opacity-100">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-maximize-2"><polyline points="15 3 21 3 21 9"/><polyline points="9 21 3 21 3 15"/><line x1="21" x2="14" y1="3" y2="10"/><line x1="3" x2="10" y1="21" y2="14"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-maximize-2"><polyline points="15 3 21 3 21 9" /><polyline points="9 21 3 21 3 15" /><line x1="21" x2="14" y1="3" y2="10" /><line x1="3" x2="10" y1="21" y2="14" /></svg>
                     </div>
                 )}
             </button>
