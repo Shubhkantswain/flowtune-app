5c794c807e5d95aeef7d6b11ba907bd840d88d3b
shubhswain45 <shubhkantswain@.com>
2025-03-19 10:45:21 +0530

    tttt

--- app/routes/_app.$username/route.tsx
+++ app/routes/_app.$username/route.tsx
@@ -147,11 +147,7 @@ const UserPage = () => {
                           <p className="text-gray-400 text-xs md:text-sm line-clamp-2 max-w-2xl">{"track.description"}</p>
                         </div>
                         <div className="hidden md:flex items-center gap-4 ml-4">
-                          <button className="group-hover:bg-white/20 backdrop-blur-sm bg-white/10 rounded-full p-1.5 md:p-2 transition-all">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><defs><path id="ic_playback_play-a" d="M21.54933,11.208 L7.32711083,2.131 C7.05155533,1.955 6.7155554,1.957 6.44177768,2.136 C6.16799996,2.315 6,2.644 6,3 L6,21 C6,21.354 6.16711108,21.683 6.43911102,21.862 C6.57777765,21.954 6.73333318,22 6.8888887,22 C7.038222,22 7.18666641,21.958 7.32177749,21.873 L21.5439967,12.951 C21.8239966,12.775 21.9991077,12.442 22,12.081 C22.0008855,11.72 21.8293299,11.386 21.54933,11.208 Z"></path></defs><g fill-rule="evenodd" fill="transparent"><rect width="24" height="24"></rect><use href="#ic_playback_play-a" fill="currentColor"></use></g></svg>
-                          </button>
-                          <div className="text-gray-400 text-xs md:text-sm w-12 text-center">{track.duration}</div>
-                          <button className="group-hover:bg-white/20 backdrop-blur-sm bg-white/10 rounded-full p-1 md:p-1.5 transition-all" onClick={() => {
+                          <button className="group-hover:bg-white/20 backdrop-blur-sm bg-white/10 rounded-full p-1.5 md:p-2 transition-all" onClick={() => {
                             const isPlayingCurrentSong = track?.id == trackDetails.id && trackDetails.isPlaying;
 
                             if (isPlayingCurrentSong && initialized) {
@@ -198,6 +194,11 @@ const UserPage = () => {
                                 <polygon points="5 3 19 12 5 21 5 3" />
                               </svg>
                             )}
+                          </button>
+                          <div className="text-gray-400 text-xs md:text-sm w-12 text-center">{track.duration}</div>
+                          <button className="group-hover:bg-white/20 backdrop-blur-sm bg-white/10 rounded-full p-1 md:p-1.5 transition-all">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-plus"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
+                           
                           </button>
                         </div>
                       </div>
