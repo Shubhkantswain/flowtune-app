075c856f776710f17fbd7524282125b34e4ff6bf
shubhswain45 <shubhkantswain@.com>
2025-04-27 17:00:57 +0530

    great

--- app/routes/_app.playlist.$id/_component/PlaylistInfo.tsx
+++ app/routes/_app.playlist.$id/_component/PlaylistInfo.tsx
@@ -122,7 +122,7 @@ function PlaylistInfo({ res, handleControll }: PlaylistInfoProps) {
                             }}
                         >
                             {/* Tooltip inside the button */}
-                            <div className="absolute -top-7 left-1/2 ml-2 -translate-x-1/2 px-2 py-1 text-xs bg-zinc-800 text-white shadow-lg 
+                            <div className="absolute -top-7 left-1/2 ml-0.5 -translate-x-1/2 px-2 py-1 text-xs bg-zinc-800 text-white shadow-lg 
                             opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap border border-white">
                                 Share
                             </div>
@@ -141,7 +141,7 @@ function PlaylistInfo({ res, handleControll }: PlaylistInfoProps) {
                             aria-label="More options"
                         >
                             {/* Tooltip inside the button */}
-                            <div className="absolute -top-7 left-1/2 ml-2 -translate-x-1/2 px-2 py-1 text-xs bg-zinc-800 text-white shadow-lg 
+                            <div className="absolute -top-7 left-1/2 ml-0.5 -translate-x-1/2 px-2 py-1 text-xs bg-zinc-800 text-white shadow-lg 
                             opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap border border-white">
                                 More
                             </div>
